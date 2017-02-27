bdd3ede141cd0426f70843fda5efd8080283cbdf
Louis Tran <loutran987@gmail.com>
2017-02-26 23:19:20 -0800

    Simplified solution for Python If-Else

--- python/introduction/if-else.py
+++ python/introduction/if-else.py
@@ -4,9 +4,7 @@ if __name__ == '__main__':
 if (n % 2 == 1) :
     print("Weird")
 else:
-    if (n >= 2 and n <= 5):
-        print("Not Weird")
-    elif (n >= 6 and n <= 20):
+    if (n >= 6 and n <= 20):
         print("Weird")
-    elif (n > 20):
+    else:
         print("Not Weird")
