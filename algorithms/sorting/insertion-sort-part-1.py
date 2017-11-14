3c4459fcf464e7afc7bce9871312b00da88d21d4
Louis Tran <loutran987@gmail.com>
2017-11-14 00:07:06 -0800

    Redo Insertion Sort Part 1

--- algorithms/sorting/insertion-sort-part-1.py
+++ algorithms/sorting/insertion-sort-part-1.py
@@ -1,20 +1,14 @@
 #!/bin/python
 def insertionSort(ar):
-    i = 1
-    prev = ar[0]
-    while (i < len(ar)):
-        j = i
-        if (ar[i] > prev):
-            prev = ar[i]
-        else:
-            temp = ar[i]
-            while (j > 0 and not (ar[j-1] < temp and ar[j] >= temp)):
-                ar[j] = ar[j-1]
-                j -= 1
-                print(' '.join(str(x) for x in ar))
-            ar[j] = temp
-            print(' '.join(str(x) for x in ar))
-        i += 1
+    j = len(ar)-1
+    key = ar[j]
+    while (j>0 and ar[j-1]>key):
+        ar[j] = ar[j-1]
+        j -= 1
+        print(' '.join(str(x) for x in ar))
+    ar[j] = key
+    print(' '.join(str(x) for x in ar))
+
 
 m = input()
 ar = [int(i) for i in raw_input().strip().split()]
